11de337c06f070f62492a9843300f1953b64deaf
Joseph <wifiandroi@gmail.com>
2022-09-18 23:34:21 -0500

    Agregando el Controlador

--- src/main/java/com/app/angular/controller/ActividadController.java
+++ src/main/java/com/app/angular/controller/ActividadController.java
@@ -1,8 +1,51 @@
 package com.app.angular.controller;
 
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ActividadController {
+import com.app.angular.dto.ActividadDto;
+import com.app.angular.entity.Actividad;
+import com.app.angular.service.ActividadService;
 
+@RestController
+@RequestMapping("/actividad")
+public class ActividadController {
+	
+	@Autowired
+	ActividadService actividadService;
+	
+	@GetMapping
+	public ResponseEntity<List<Actividad>> getAll(){
+		return ResponseEntity.ok(actividadService.getAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Actividad> getOne(@PathVariable("id") int id){
+		return ResponseEntity.ok(actividadService.getOne(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Actividad> save(@RequestBody ActividadDto dto){
+		return ResponseEntity.ok(actividadService.save(dto));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Actividad> update(@PathVariable("id") int id, @RequestBody ActividadDto dto){
+		return ResponseEntity.ok(actividadService.update(id, dto));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Actividad> delete(@PathVariable("id") int id){
+		return ResponseEntity.ok(actividadService.delete(id));
+	}
 }
