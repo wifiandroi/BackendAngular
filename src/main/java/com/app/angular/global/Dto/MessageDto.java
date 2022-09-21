7d9689ceda4d3eaefe04343f76e1b7f64933e291
Joseph <wifiandroi@gmail.com>
2022-09-20 21:27:14 -0500

    Incorporación de als clases  "MessageDto y GlobalException", el caul permitira mejorar el mensaje de errores

---
+++ src/main/java/com/app/angular/global/Dto/MessageDto.java
@@ -0,0 +1,35 @@
+package com.app.angular.global.Dto;
+
+import org.springframework.http.HttpStatus;
+
+public class MessageDto {
+
+	private HttpStatus status;
+	private String message;
+
+	public MessageDto(HttpStatus status, String message) {
+		this.status = status;
+		this.message = message;
+	}
+
+	public MessageDto() {
+
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ src/main/java/com/app/angular/global/exceptions/GlobalException.java
@@ -0,0 +1,20 @@
+package com.app.angular.global.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.app.angular.global.Dto.MessageDto;
+
+
+@RestControllerAdvice
+public class GlobalException {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<MessageDto> throwNotFoundException(ResourceNotFoundException e){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageDto(HttpStatus.NOT_FOUND, e.getMessage()));
+	}
+	
+
+}
